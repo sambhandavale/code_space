a69311a603016b63caa754282ca84201dc4e5f75
Samarth Bhandavale <sambhandavale15@gmail.com>
2025-04-21 19:57:19 +0530

    changes

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -3,6 +3,10 @@ import react from '@vitejs/plugin-react-swc'
 
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: '../dist',
+    emptyOutDir: true,
+  },
   server:{
     port: 1507,
   }
