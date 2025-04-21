0e2fe8dd169b7c6efac6d391e780e58595b48f6d
Samarth Bhandavale <sambhandavale15@gmail.com>
2025-04-21 21:06:52 +0530

    update auth

--- client/src/components/Authentication/AuthContainer.tsx
+++ client/src/components/Authentication/AuthContainer.tsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { postAction } from "../../services/generalServices";
-import { authenticate } from "../../utility/helper";
+import { authenticate, setLocalStorage } from "../../utility/helper";
 import { toast } from 'sonner';
 
 interface IAuthContainer{
@@ -55,8 +55,10 @@ const AuthContainer = ({
         try {
             const data = { email: email, password: password };
             const res = await postAction(route,data,informParent);
+            console.log(res);
             if(res.status === 200){
                 toast.success(`Welcome, ${res.data.user.first_name} ${res.data.user.last_name}`);
+                setLocalStorage('token',res.data.jwtToken);
             }
         } catch (err) {
             console.error("Failed Authentication, ", err);
