83c81e9f86e06a30ab5090b96dc46fe0b5dc8a03
Samarth Bhandavale <sambhandavale15@gmail.com>
2025-04-22 18:58:39 +0530

    time issue solved

--- client/src/containers/Challenge/ChallengeRoom.tsx
+++ client/src/containers/Challenge/ChallengeRoom.tsx
@@ -34,7 +34,7 @@ const ChallengeRoom = () => {
     const [socketId, setSocketId] = useState<string | null | undefined>(null);
     const [challengeDetails,setChallengeDetails] = useState<IChallenge>()
     const [dividerPosition, setDividerPosition] = useState(50);
-    const [timeLeft, setTimeLeft] = useState<number>(1);
+    const [timeLeft, setTimeLeft] = useState<number>(10);
     const timerRef = useRef<NodeJS.Timeout | null>(null);
 
     const containerRef = useRef<HTMLDivElement | null>(null);
@@ -59,8 +59,7 @@ const ChallengeRoom = () => {
 
     useEffect(() => {
         if (challengeDetails?.time) {
-            // setTimeLeft(challengeDetails.problem_id.time * 60);
-            setTimeLeft(200 * 60);
+            setTimeLeft(challengeDetails.problem_id.time * 60);
         }
     }, [challengeDetails]);
     
