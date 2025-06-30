4132a412ce57249fd4c5bc2bc39a87ce53056760
Samarth Bhandavale <sambhandavale15@gmail.com>
2025-06-30 23:36:57 +0530

    cors update

--- App.ts
+++ App.ts
@@ -32,6 +32,8 @@ app.use(
       origin: [
         process.env.REACT_APP_BASE_URL,
         process.env.REACT_APP_BASE_URL2,
+        process.env.REACT_APP_BASE_URL3,
+        'https://www.code-space.tech',
         'https://code-space.tech',
       ],
     }),
